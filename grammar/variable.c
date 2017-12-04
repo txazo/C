efbc4ab8ad7d8ac84c6ce0d098b8a97069e994dd
txazo <784990655@qq.com>
2017-12-04 19:32:19 +0800

    update

--- grammar/variable.c
+++ grammar/variable.c
@@ -16,58 +16,49 @@ static void test6(void);
  * 变量
  */
 int main(int argc, char *argv[]) {
-    char a = '1';
-    int b = 2;
-    long c = 3;
-    int *d = &a;
-    printf("a [%p]=%c\n", &a, a);
-    printf("b [%p]=%d\n", &b, b);
-    printf("c [%p]=%d\n", &c, c);
-    printf("d [%p]=%p\n", &d, d);
-
     printf("-------------------test1---------------------\n");
     test1();
-    printf("-------------------test2---------------------\n");
+    printf("\n-------------------test2---------------------\n");
     test2();
-    printf("-------------------test3---------------------\n");
+    printf("\n-------------------test3---------------------\n");
     test3();
-    printf("-------------------test4---------------------\n");
+    printf("\n-------------------test4---------------------\n");
     test4();
-    printf("-------------------test5---------------------\n");
+    printf("\n-------------------test5---------------------\n");
     test5();
-    printf("-------------------test6---------------------\n");
+    printf("\n-------------------test6---------------------\n");
     test6();
-
     return 0;
 }
 
 static void test1(void) {
-    int p = 1;
-    printf("[%p]=%d\n", &p, p);
+    int i = 1;
+    printf("[%p] = %d\n", &i, i);
 }
 
 static void test2(void) {
     int a = 2;
     int *p = &a;
-    printf("[%p]=%p\n", &p, p);
-    printf("[%p]=%d\n", &*p, *p);
-    printf("[%p]=%d\n", &a, a);
+    printf("[%p] = %p\n", &p, p);
+    printf("[%p] = %d\n", &*p, *p);
+    printf("[%p] = %d\n", &a, a);
 }
 
 static void test3(void) {
     int a = 3;
     int *b = &a;
     int **p = &b;
-    printf("[%p]=%p\n", &p, p);
-    printf("[%p]=%p\n", &*p, *p);
-    printf("[%p]=%d\n", &**p, **p);
+    printf("[%p] = %p\n", &p, p);
+    printf("[%p] = %p\n", &*p, *p);
+    printf("[%p] = %d\n", &**p, **p);
 }
 
 static void test4(void) {
-    int p[3] = {1, 2, 3};
-    printf("[%p]=%d\n", &p[0], p[0]);
-    printf("[%p]=%d\n", &p[1], p[1]);
-    printf("[%p]=%d\n", &p[2], p[2]);
+    int a[3] = {1, 2, 3};
+    printf("[%p]\n", a);
+    printf("[%p] = %d\n", &a[0], a[0]);
+    printf("[%p] = %d\n", &a[1], a[1]);
+    printf("[%p] = %d\n", &a[2], a[2]);
 }
 
 static void test5(void) {
@@ -78,20 +69,20 @@ static void test5(void) {
     p[0] = &a;
     p[1] = &b;
     p[2] = &c;
-    printf("[%p]=%p\n", &p[0], p[0]);
-    printf("[%p]=%p\n", &p[1], p[1]);
-    printf("[%p]=%p\n", &p[2], p[2]);
-    printf("[%p]=%d\n", &*p[0], *p[0]);
-    printf("[%p]=%d\n", &*p[1], *p[1]);
-    printf("[%p]=%d\n", &*p[2], *p[2]);
+    printf("[%p] = %p\n", &p[0], p[0]);
+    printf("[%p] = %p\n", &p[1], p[1]);
+    printf("[%p] = %p\n", &p[2], p[2]);
+    printf("[%p] = %d\n", &*p[0], *p[0]);
+    printf("[%p] = %d\n", &*p[1], *p[1]);
+    printf("[%p] = %d\n", &*p[2], *p[2]);
 }
 
 static void test6(void) {
     int a[3] = {1, 2, 3};
     int (*p)[3];
     p = &a;
-    printf("[%p]=%p\n", &p, p);
-    printf("[%p]=%d\n", &(*p)[0], (*p)[0]);
-    printf("[%p]=%d\n", &(*p)[1], (*p)[1]);
-    printf("[%p]=%d\n", &(*p)[2], (*p)[2]);
+    printf("[%p] = %p\n", &p, p);
+    printf("[%p] = %d\n", &(*p)[0], (*p)[0]);
+    printf("[%p] = %d\n", &(*p)[1], (*p)[1]);
+    printf("[%p] = %d\n", &(*p)[2], (*p)[2]);
 }
